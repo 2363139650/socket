6573f2b1ba9a2aa219c190d4f8f94a70b4b09458
dulinchao <780446681@qq.com>
2021-11-20 22:26:31 +0800

    李佳的优质代码

--- client1.py
+++ client1.py
@@ -27,18 +27,36 @@ def get_file():
             recv_size += len(lene)
             print('总大小：%s  已下载大小:%s' % (total_size, recv_size))  # 打印下载进度
 
+def put_file():
+    '''接收文件的内容，以写的方式打开一个新文件并写入来实现下载功能'''
+    # 获取包头长
+    header_size = struct.unpack('i', soc.send(4))[0]
+    # 获得包头
+    header_dict = json.loads(soc.send(header_size).decode('GBK'))
+    # 解析包头信息 和 文件夹命名
+    total_size = header_dict.get('file_size')
+    file_path = '%s/%s' % (download_dir, header_dict.get('file_name'))
+    print(file_path)
+    # 写入到新的文件
+    with open(file_path, 'rb') as f:  # 必须要用二进制模式打开
+        send_size = 0
+        while send_size < total_size:
+            lene = soc.send(1024)
+            f.read(lene)
+            send_size += len(lene)
+            print('总大小：%s  已上传大小:%s' % (total_size, send_size))  # 打印下载进度
+
 
 while True:
     # 发送命令
     cmd = input('>>>').strip()  # get a.txt
-    if cmd == 'put .*':
+    if cmd.startswith('put'):
         soc.send(cmd.encode('utf-8'))
-    elif cmd == 'get .*':
+        put_file()
+    elif cmd.startswith('get'):
         soc.send(cmd.encode('utf-8'))
+        get_file()
     elif not cmd:
         continue
-    soc.send(cmd.encode('utf-8'))
-
-    get_file()
 
 soc.close()
